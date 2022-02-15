442f21525ca6ca78116a3f4d832f63c0a238ba47
LopA <65017474+Gitnuts@users.noreply.github.com>
2022-02-15 13:38:16 +0200

    Update kafka_producer.py

--- Kafka_code/kafka_producer.py
+++ Kafka_code/kafka_producer.py
@@ -2,15 +2,14 @@ from kafka import KafkaProducer
 import json
 import time
 
-def json_serializer(kafka_d):
-	return json.dumps(kafka_d).encode("utf-8")
+def json_serializer(kafka_data):
+	return json.dumps(kafka_data).encode("utf-8")
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-						 value_serializer=json_serializer,
-						 api_version=(0,10,1))
+producer = KafkaProducer(bootstrap_servers=['kafka:9092'],
+			value_serializer=json_serializer,
+			api_version=(1,4,6))
 
-
-with open("input_file.json") as f:
+with open("/usr/local/airflow/data/input_file.json") as f:
 	data_object = json.load(f)	
 	for odd in data_object['odds']:
 		producer.send("test",odd)
