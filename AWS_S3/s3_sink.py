33b01d5cad54edc40d471a92670b3046335805c8
LopA <65017474+Gitnuts@users.noreply.github.com>
2022-02-15 14:19:53 +0200

    Update s3_sink.py

--- AWS_S3/s3_sink.py
+++ AWS_S3/s3_sink.py
@@ -1,4 +1,12 @@
 import boto3 
+import glob
+
+path = 'path/to/the/folder' # path to the folder with files outputted from Structured Streaming in docker
+text_file = glob.glob(path + "/*.csv", recursive=True)
 
 s3_resourse = boto3.resource('s3')
-# s3_resourse.create_bucket(Bucket='bookmakerdata1', CreateBucketConfiguration={'LocationConstraint': 'eu-north-1'}) creating s3 bucket if not created
+
+# uncomment if bucket is not created yet and enter the name for a new bucket 
+#s3_resourse.create_bucket(Bucket='bucket_name', CreateBucketConfiguration={'LocationConstraint': 'eu-north-1'})
+
+s3_resourse.Object('bucket_name', text_file[0]).upload_file(Filename = text_file[0])
