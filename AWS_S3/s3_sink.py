162930a371074726e312c7ae42af807e41d72f78
LopA <65017474+Gitnuts@users.noreply.github.com>
2022-02-24 18:05:19 +0200

    Update s3_sink.py

--- AWS_S3/s3_sink.py
+++ AWS_S3/s3_sink.py
@@ -1,12 +1,28 @@
 import boto3 
+import os
+import shutil
 import glob
+import datetime
 
-path = 'path/to/the/folder' # path to the folder with files outputted from Structured Streaming in docker
-text_file = glob.glob(path + "/*.csv", recursive=True)
+path = '/usr/local/airflow'
+text_file = glob.glob(path + "/*.parquet", recursive=True)
+
+
+print(text_file)
+
+x = datetime.datetime.now()
+x = str(x)
+x = x[:10]
+
+for content in text_file:
+	os.rename(content, path + "/" + x + ".parquet")
+
+print(text_file)
+text_file = glob.glob(path + "/*.parquet", recursive=True)
 
 s3_resourse = boto3.resource('s3')
+#s3_resourse.create_bucket(Bucket='bookmakerdata1', CreateBucketConfiguration={'LocationConstraint': 'eu-north-1'})
+s3_resourse.Object('bookmakerdata1', path + "/" + x + ".parquet" ).upload_file(Filename = path + "/" + x + ".parquet")
 
-# uncomment if bucket is not created yet and enter the name for a new bucket 
-#s3_resourse.create_bucket(Bucket='bucket_name', CreateBucketConfiguration={'LocationConstraint': 'eu-north-1'})
-
-s3_resourse.Object('bucket_name', text_file[0]).upload_file(Filename = text_file[0])
+for content in text_file:
+	os.remove(content)
